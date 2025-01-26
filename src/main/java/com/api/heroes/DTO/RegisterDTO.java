a3f691b9398431f26966de6495ce568f240bd094
gonzaviale <gonzaloleonelviale@gmail.com>
2025-01-25 21:38:30 -0300

    feature/dto

--- src/main/java/com/api/heroes/DTO/RegisterDTO.java
+++ src/main/java/com/api/heroes/DTO/RegisterDTO.java
@@ -1,7 +1,11 @@
 package com.api.heroes.DTO;
 
 import com.api.heroes.models.enumerators.Role;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class RegisterDTO {
     private String name;
     private String email;
@@ -11,59 +15,4 @@ public class RegisterDTO {
     private BankDTO bank;
     private VeterinarianDTO veterinarian;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public BankDTO getBank() {
-        return bank;
-    }
-
-    public void setBank(BankDTO bank) {
-        this.bank = bank;
-    }
-
-    public VeterinarianDTO getVeterinarian() {
-        return veterinarian;
-    }
-
-    public void setVeterinarian(VeterinarianDTO veterinarian) {
-        this.veterinarian = veterinarian;
-    }
 }
