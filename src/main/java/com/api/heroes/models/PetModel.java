e092a333ca088e30c5155af48d34661999427190
gonzaviale <gonzaloleonelviale@gmail.com>
2025-01-25 21:39:13 -0300

    feature/veterinarian-and-pet-model

--- src/main/java/com/api/heroes/models/PetModel.java
+++ src/main/java/com/api/heroes/models/PetModel.java
@@ -3,7 +3,11 @@ package com.api.heroes.models;
 import com.api.heroes.models.enumerators.Status;
 import com.api.heroes.models.enumerators.TypeOfPet;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "pet")
 public class PetModel {
@@ -25,76 +29,5 @@ public class PetModel {
     @JoinColumn(name = "owner_id")
     private UserModel owner;
 
-    public String getWeight() {
-        return weight;
-    }
 
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getDiagnosis() {
-        return diagnosis;
-    }
-
-    public void setDiagnosis(String diagnosis) {
-        this.diagnosis = diagnosis;
-    }
-
-    public String getBloodType() {
-        return bloodType;
-    }
-
-    public void setBloodType(String bloodType) {
-        this.bloodType = bloodType;
-    }
-
-    public TypeOfPet getType() {
-        return type;
-    }
-
-    public void setType(TypeOfPet type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public UserModel getOwner() {
-        return owner;
-    }
-
-    public void setOwner(UserModel owner) {
-        this.owner = owner;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
--- src/main/java/com/api/heroes/models/VeterinarianModel.java
+++ src/main/java/com/api/heroes/models/VeterinarianModel.java
@@ -2,7 +2,11 @@ package com.api.heroes.models;
 
 import com.api.heroes.models.enumerators.Status;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "veterinarian")
 public class VeterinarianModel {
@@ -18,51 +22,4 @@ public class VeterinarianModel {
     private UserModel user;
     private Integer score;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLicense() {
-        return license;
-    }
-
-    public void setLicense(String license) {
-        this.license = license;
-    }
-
-    public String getFileLicense() {
-        return fileLicense;
-    }
-
-    public void setFileLicense(String fileLicense) {
-        this.fileLicense = fileLicense;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public UserModel getUser() {
-        return user;
-    }
-
-    public void setUser(UserModel user) {
-        this.user = user;
-    }
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
-    }
 }
