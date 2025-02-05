86dee6727e5f2d0ea2b4238b037bcb26a0506e81
gonzaviale <gonzaloleonelviale@gmail.com>
2025-02-04 22:13:47 -0300

    feat: WebSocketConfig

--- src/main/java/com/api/heroes/config/WebConfig.java
+++ src/main/java/com/api/heroes/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.api.heroes.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Cambia al origen de tu frontend
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
